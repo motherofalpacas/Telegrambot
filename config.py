4b66e9521c1b9d09312acade731ee1ba6d078c1a
Olha Yatsiuk <80070848+motherofalpacas@users.noreply.github.com>
2022-06-02 08:41:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = "5400659841:AAEVWWQxNq929pACJ6MPZAN3nldbLS-j22E"
 BOT_ENDPOINT = "83ceda2f-f193-4142-9559-d8c96f8da532"
 DATABASE_URL = 'sqlite:///Questions.db'
-APP_URL = "https://marketing-telegram-bot-228.herokuapp.com/" + BOT_ENDPOINT
+APP_URL = "https://telegrambotbyyatsiuk.herokuapp.com/" + BOT_ENDPOINT
 TESTS_AMOUNT = 5
